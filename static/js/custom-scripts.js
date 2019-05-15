72e7a211b2a659ee90f0dfd00155e49230a12b53
Lukasz Malucha <lucasmalucha@gmail.com>
2019-05-15 15:47:29 +0100

    style

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -5,17 +5,16 @@ $('.dropdown-trigger').dropdown();
 $(".alert-user").delay(3000).fadeOut(200, function() {
     $(this).alert('close');
 });
-var nyTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
-var today = new Date(nyTime);
 
-var clock = $('.us-clock').FlipClock(today, {
-    clockFace: 'TwelveHourClock'
-});
 
 
 
 $(document).ready(function() {
 
+    $('#dataTable').dataTable({
+        paging: false,
+        searching: false
+    });
 
     $('select').formSelect();
 
